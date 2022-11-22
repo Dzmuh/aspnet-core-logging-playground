6eb8a151983db95e2f11667f99ab26f1ca021e18
Ilya Fralkou <dzmuh@outlook.com>
2022-11-22 11:34:43 +0100

    Serilog и .NET 7

---
+++ dotnet7-serilog/aspnet-core-serilog-inmain/Program.cs
@@ -0,0 +1,48 @@
+using Serilog;
+
+namespace aspnet_core_serilog_inmain
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                             .WriteTo.Console()
+                             .CreateBootstrapLogger();
+
+            Log.Information("Starting up");
+
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog((context, services, configuration) => configuration
+                            .WriteTo.Console()
+                            .ReadFrom.Configuration(context.Configuration)
+                            .ReadFrom.Services(services));
+
+                var app = builder.Build();
+
+                app.UseSerilogRequestLogging();
+
+                app.MapGet("/", () => "Hello World!");
+                app.MapGet("/oops", new Func<string>(() => throw new InvalidOperationException("Oops!")));
+
+                app.Run();
+
+                Log.Information("Stopped cleanly");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+                return 1;
+            }
+            finally
+            {
+                Log.Information("Shut down complete");
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
