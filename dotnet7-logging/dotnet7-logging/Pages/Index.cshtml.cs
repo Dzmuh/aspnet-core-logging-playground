eb0349126874a9a719b1cae76992e631f4b1ab8d
Ilya Fralkou <dzmuh@outlook.com>
2022-11-25 12:47:02 +0100

    Общие доработки.

--- dotnet7-logging/dotnet7-logging/Pages/Index.cshtml.cs
+++ dotnet7-logging/dotnet7-logging/Pages/Index.cshtml.cs
@@ -5,16 +5,40 @@ namespace dotnet7_logging.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        // public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILoggerFactory factory)
         {
-            _logger = logger;
+            //_logger = logger;
+            this._logger = factory.CreateLogger("CustomCategory");
         }
 
         public void OnGet()
         {
+            _logger.LogTrace("TRACE.");
+            _logger.LogDebug("DEBUG.");
+            _logger.LogInformation("INFORMATION.");
+            _logger.LogWarning("WARNING.");
+            _logger.LogError("ERROR.");
+            _logger.LogCritical("CRITICAL.");
 
+            _logger.LogError("Server went down temporarily at {Time}", DateTime.UtcNow);
+
+            try
+            {
+                throw new Exception("Unhandled");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "There was a bad exception at {Time}", DateTime.UtcNow);
+            }
+        }
+
+        public class LoggingId
+        {
+            public const int DemoCode = 1001;
         }
     }
 }
--- dotnet7-serilog/aspnet-core-serilog-details/Program.cs
+++ dotnet7-serilog/aspnet-core-serilog-details/Program.cs
@@ -1,15 +1,51 @@
+using Serilog;
+using Serilog.Events;
+
 namespace aspnet_core_serilog_details
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            Log.Logger = new LoggerConfiguration()
+                             .WriteTo.Console()
+                             .CreateBootstrapLogger();
 
-            app.MapGet("/", () => "Hello World!");
+            Log.Information("Starting up");
 
-            app.Run();
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+
+                const string outputTemplate = "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}";
+
+                builder.Host.UseSerilog((context, services, configuration) => configuration
+                            .MinimumLevel.Information()
+                            .MinimumLevel.Override("System", LogEventLevel.Error)
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console(outputTemplate: outputTemplate));
+
+                var app = builder.Build();
+
+                app.MapGet("/", () => "Hello World!");
+
+                app.Run();
+
+                Log.Information("Stopped cleanly");
+                return 1;
+            }
+            catch (Exception ex)
+            {
+
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+                return 0;
+            }        
+            finally
+            {
+                Log.Information("Shut down complete");
+                Log.CloseAndFlush();
+            }            
         }
     }
 }
